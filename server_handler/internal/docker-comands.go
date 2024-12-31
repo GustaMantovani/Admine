5fad3248e013e49947cb9ea142ae0a1c66340187
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2024-12-31 10:27:38 -0300

    :sparkles: puts the message sent to pubsub in the project standard

--- server_handler/internal/docker-comands.go
+++ server_handler/internal/docker-comands.go
@@ -1,6 +1,9 @@
 package internal
 
-import "os/exec"
+import (
+	"os/exec"
+	"strings"
+)
 
 // Executa o comando 'docker compose up -d' em determinado diretorio e retorna o output do cmd
 func StartServerDockerCompose(composeDirectory string) ([]byte, error) {
@@ -8,4 +11,19 @@ func StartServerDockerCompose(composeDirectory string) ([]byte, error) {
 	cmd.Dir = composeDirectory
 
 	return cmd.CombinedOutput()
+}
+
+func GetZeroTierNodeID() string {
+	cmd := exec.Command("docker", "exec", "-i", "minecraft-server-mine_server-1", "/bin/bash", "-c", "zerotier-cli info")
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		panic(err)
+	}
+
+	outputStr := string(output)
+
+	parts := strings.Split(outputStr, " ")
+
+	return parts[2]
 }
