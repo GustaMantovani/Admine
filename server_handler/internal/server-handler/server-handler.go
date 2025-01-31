80815acf65944218834ddbd0dc52f9dcd28a2a68
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2025-01-31 10:57:37 -0300

    :recycle: remove hardcoded server properties

--- server_handler/internal/server-handler/server-handler.go
+++ server_handler/internal/server-handler/server-handler.go
@@ -3,10 +3,11 @@ package serverhandler
 import (
 	"log"
 	"server/handler/internal/docker"
+	minecraftserver "server/handler/internal/minecraft-server"
 	"server/handler/internal/pubsub"
 )
 
-func RunServerHandler() {
+func RunServerHandler(ms minecraftserver.MinecraftServerContainerByCompose) {
 	config := pubsub.GetConfigServerChannelFromDotEnv("HEALTH_CHECKER_CHANNEL")
 
 	c := make(chan string)
@@ -18,7 +19,7 @@ func RunServerHandler() {
 		msg = <-c
 		if msg == "down" {
 			log.Println("Subindo servidor")
-			res, err := docker.StartServerDockerCompose("/home/andre/pgm/pessoal/Admine/minecraft-server/")
+			res, err := docker.StartServerDockerCompose(ms.ComposeDirectoryFullName)
 			if err != nil {
 				log.Fatal(err)
 			}
