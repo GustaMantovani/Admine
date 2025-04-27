e755061e5a806e3cce5eb9388e172f13b6b28ec2
Gustavo Mantovani <mantovanig000@gmail.com>
2025-04-27 16:36:15 -0300

    🚚 rename minecraft info service files for consistency and clarity

--- bot/core/external/providers/minecraft_server_info_service_providers/minecraft_server_info_service_factory.py
+++ bot/core/external/providers/minecraft_server_info_service_providers/minecraft_server_info_service_factory.py
@@ -1,7 +1,7 @@
 from typing import Callable, Dict, Any
 from logging import Logger
 from core.external.abstractions.minecraft_server_info_service import MinecraftServerInfoService
-from core.external.providers.minecraft_server_info_service_providers.minecraft_info_service_provider_type import MinecraftInfoServiceProviderType
+from core.external.providers.minecraft_server_info_service_providers.minecraft_server_info_service_provider_type import MinecraftInfoServiceProviderType
 from core.config import Config
 from core.exceptions import MinecraftInfoServiceFactoryException
 
