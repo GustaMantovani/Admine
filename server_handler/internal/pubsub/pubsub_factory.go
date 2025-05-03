31f5c8dc8606420ecb9506a54354af47f9a3997d
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2025-05-02 21:49:21 -0300

    :sparkles: add a new type of pubsub for tests

--- server_handler/internal/pubsub/pubsub_factory.go
+++ server_handler/internal/pubsub/pubsub_factory.go
@@ -1,7 +1,8 @@
 package pubsub
 
 var pubSubTypes = map[string]PubSubInterface{
-	"redis": New("127.0.0.1:6379", "teste"),
+	"redis":        New("127.0.0.1:6379", "teste"),
+	"redis-sender": New("127.0.0.1:6379", "teste-sender"),
 }
 
 func PubSubFactoryCreate(pubsub string) PubSubInterface {
