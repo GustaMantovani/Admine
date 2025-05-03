866eaaf30422c1f5d29a5b184c5a77e62a537d99
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2025-05-02 21:52:40 -0300

    :recycle: move to cli queue the responsability to pass the pubsub command to the handler

--- server_handler/cmd/queue/queue.go
+++ server_handler/cmd/queue/queue.go
@@ -2,16 +2,44 @@ package queue
 
 import (
 	"log"
+	"server_handler/internal/handler"
+	"server_handler/internal/message"
 	"server_handler/internal/pubsub"
 )
 
+/*
+Start to listen a pubsub for commands
+*/
 func RunListenQueue() {
+	listenCommands()
+}
+
+/*
+Define two threads.
+
+One for listen the pubsub and other to send commands to handler.
+*/
+func listenCommands() {
 	psType := "redis"
 	ps := pubsub.PubSubFactoryCreate(psType)
+
+	psType = "redis-sender"
+	pss := pubsub.PubSubFactoryCreate(psType)
 
 	if ps == nil {
 		log.Fatal("Tipo de PubSub não existe: ", psType)
 	}
 
-	ps.ListenForMessages()
+	// message channel
+	mc := make(chan message.Message)
+
+	go ps.ListenForMessages(mc)
+
+	for msg := range mc {
+		log.Println(msg)
+		if len(msg.Tags) > 0 {
+			handler.ManageCommand(msg.Tags[0])
+		}
+		pss.SendMessage("comando enviado")
+	}
 }
