93d96191cf4e206929f26a5ef5d9b16b1c46ae34
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2025-05-04 23:18:02 -0300

    :sparkles: make handler stop server before down the container and add a command 'ping' that give response to all players 'PONG'

--- server_handler/internal/handler/handler.go
+++ server_handler/internal/handler/handler.go
@@ -4,17 +4,23 @@ import (
 	"errors"
 	"log"
 	"os/exec"
+	commandhandler "server_handler/internal/command_handler"
 	"server_handler/internal/server"
 	"strings"
+	"time"
 )
 
 func ManageCommand(command string) error {
-	if command == "start" {
+	if command == "start_server" {
 		server.StartServerCompose()
-		log.Println("Up server compose")
-	} else if command == "stop" {
+		log.Println("Start server")
+	} else if command == "stop_server" {
+		commandhandler.WriteToContainer("/stop")
+		time.Sleep(5 * time.Second)
 		server.StopServerCompose()
-		log.Println("Down server compose")
+		log.Println("Stop server")
+	} else if command == "ping" {
+		commandhandler.WriteToContainer("/say PONG")
 	} else {
 		return errors.New("invalid command")
 	}
