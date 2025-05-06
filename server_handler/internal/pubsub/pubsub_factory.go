2eaf263c1eba0b24ac557167faeaef57fa91ba2b
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2025-05-06 11:06:02 -0300

    :sparkles: factory get sender channel from Config

--- server_handler/internal/pubsub/pubsub_factory.go
+++ server_handler/internal/pubsub/pubsub_factory.go
@@ -6,7 +6,7 @@ var c = config.GetInstance()
 
 var pubSubTypes = map[string]PubSubInterface{
 	"redis-consumer": New("127.0.0.1:6379", c.ConsumerChannel),
-	"redis-sender":   New("127.0.0.1:6379", "teste-sender"),
+	"redis-sender":   New("127.0.0.1:6379", c.SenderChannel),
 }
 
 func PubSubFactoryCreate(pubsub string) PubSubInterface {
