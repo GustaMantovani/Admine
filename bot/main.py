1e8bef9c061f55db7aa56bc2eac1f899b22791bf
Gustavo Mantovani <mantovanig000@gmail.com>
2025-05-11 10:25:36 -0300

    🐛 add dotenv load

--- bot/main.py
+++ bot/main.py
@@ -6,31 +6,21 @@ from core.logger import CustomLogger
 from core.exceptions import ConfigError, ConfigFileError
 import asyncio
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def main():
 
     logger = CustomLogger(logger_name="Admine Bot", log_file="/tmp/bot.log")
 
     try:
-        try:
-            config = Config()
-        except ConfigFileError as e:
-            logger.get_logger().error(f"Configuration file error: {e}")
-            sys.exit(1)
-        except ConfigError as e:
-            logger.get_logger().error(f"Configuration error: {e}")
-            sys.exit(1)
-
+        config = Config()
         bot = Bot(logger.get_logger(), config)
-
-
         bot.start()
-        
     except Exception as e:
         logger.get_logger().error(f"Unexpected error: {e}\n{traceback.format_exc()}")
         sys.exit(1)
-
-
 
 if __name__ == "__main__":
     main()
