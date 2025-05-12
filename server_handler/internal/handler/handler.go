560a0f7442954a3d1239948057d3db1c3bfd1a04
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2025-05-12 09:48:33 -0300

    :recycle: move some code to functions, make config a package-level-scope variable

--- server_handler/internal/handler/handler.go
+++ server_handler/internal/handler/handler.go
@@ -10,50 +10,55 @@ import (
 	"strings"
 )
 
+var c *config.Config = config.GetInstance()
+
 func ManageCommand(tag, message string, ps pubsub.PubSubInterface) error {
-	c := config.GetInstance()
-
-	if tag == "start_server" {
-
-		server.StartServerCompose()
-		ps.SendMessage("Starting server", c.SenderChannel)
-		ps.SendMessage(docker.GetZeroTierNodeID(c.ComposeContainerName), c.SenderChannel)
-		log.Println("Start server")
-
-	} else if tag == "stop_server" {
-
-		commandhandler.WriteToContainer("/stop")
-		ps.SendMessage("Stopping server", c.SenderChannel)
-		sair := false
-		for sair {
-			msg, err := docker.ReadLastContainerLine()
-			if err != nil {
-				log.Println("Erro ao ler a última linha do container do servidor: ", err.Error())
-			}
-			if strings.Contains(msg, "All dimensions are saved") {
-				sair = true
-			}
-		}
-		server.StopServerCompose()
-		ps.SendMessage("Server stopped", c.SenderChannel)
-		ps.SendMessage(docker.GetZeroTierNodeID(c.ComposeContainerName), c.SenderChannel)
-
-		log.Println("Stop server")
-
+	if tag == "server_up" {
+		serverUp(ps)
+	} else if tag == "server_down" {
+		serverDown(ps)
 	} else if tag == "command" {
-
-		commandhandler.WriteToContainer(message)
-		ps.SendMessage(docker.GetZeroTierNodeID(c.ComposeContainerName), c.SenderChannel)
-
-		log.Println("Send a command to the server: ", message)
-
+		command(ps, message)
 	} else {
-
 		ps.SendMessage("Invalid tag.", c.SenderChannel)
 
 		log.Println("Received a invalid tag")
-
 	}
 
 	return nil
+}
+
+func serverUp(ps pubsub.PubSubInterface) {
+	server.StartServerCompose()
+	ps.SendMessage("Starting server", c.SenderChannel)
+	ps.SendMessage(docker.GetZeroTierNodeID(c.ComposeContainerName), c.SenderChannel)
+	log.Println("Server up")
+}
+
+func serverDown(ps pubsub.PubSubInterface) {
+	commandhandler.WriteToContainer("/stop")
+	ps.SendMessage("Stopping server", c.SenderChannel)
+
+	sair := false
+	for sair {
+		msg, err := docker.ReadLastContainerLine()
+		if err != nil {
+			log.Println("Erro ao ler a última linha do container do servidor: ", err.Error())
+		}
+		if strings.Contains(msg, "All dimensions are saved") {
+			sair = true
+		}
+	}
+
+	server.StopServerCompose()
+	ps.SendMessage("Server stopped", c.SenderChannel)
+
+	log.Println("Stop server")
+}
+
+func command(ps pubsub.PubSubInterface, message string) {
+	commandhandler.WriteToContainer(message)
+	ps.SendMessage(docker.GetZeroTierNodeID(c.ComposeContainerName), c.SenderChannel)
+
+	log.Println("Send a command to the server: ", message)
 }
