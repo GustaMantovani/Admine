ebc61ab7004209e8e40fd728d3197a9cb03c0a62
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2025-07-12 16:39:45 -0300

    :sparkles: create log configuration using slog

--- server_handler/main.go
+++ server_handler/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"server_handler/cmd"
+	"server_handler/internal/config"
 )
 
 func main() {
+	config.OpenLogFile("app.log")
+	config.CreateLogger()
+	config.GetLogger().Info("========= STARTING APP ===========")
 	cmd.Execute()
 }
