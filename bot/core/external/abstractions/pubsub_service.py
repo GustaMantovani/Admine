7be62c8051b855e84497fcd05bffdd3b3cb3729a
Gustavo Mantovani <mantovanig000@gmail.com>
2025-08-17 16:45:36 -0300

    ♻️ change default values for producer and subscribed channels lists in pubsub class

--- bot/core/external/abstractions/pubsub_service.py
+++ bot/core/external/abstractions/pubsub_service.py
@@ -3,25 +3,23 @@ from logging import Logger
 from typing import Optional, Callable
 from core.models.admine_message import AdmineMessage
 
-
-
 class PubSubService(ABC):
     def __init__(
             self,
             logging: Logger,
             host: str,
             port: int,
-            subscribed_channels: Optional[list[str]] = None,
-            producer_channels: Optional[list[str]] = None,
+            subscribed_channels: Optional[list[str]] = [],
+            producer_channels: Optional[list[str]] = [],
     ):
         self._logger = logging
         self.__host = host
         self.__port = port
         self.__subscribed_channels = (
-            subscribed_channels if subscribed_channels is not None else []
+            subscribed_channels
         )
         self.__producer_channels = (
-            producer_channels if producer_channels is not None else []
+            producer_channels
         )
 
     @property
