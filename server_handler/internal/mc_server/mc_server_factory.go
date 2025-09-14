bdd550fa30b522e3dff667aafecee4993c5962ca
Gustavo Mantovani <mantovanig000@gmail.com>
2025-09-14 17:09:40 -0300

    ♻️ refactor config usage in mc_server_factory

--- server_handler/internal/mc_server/mc_server_factory.go
+++ server_handler/internal/mc_server/mc_server_factory.go
@@ -3,23 +3,17 @@ package mcserver
 import (
 	"fmt"
 
-	"admine.com/server_handler/internal"
+	"admine.com/server_handler/internal/config"
 	mcserver "admine.com/server_handler/internal/mc_server/mc_server_impls"
 	"admine.com/server_handler/pkg"
 )
 
-func CreateMinecraftServer(serverType string) (MinecraftServer, error) {
-	ctx := internal.Get() // singleton AppContext
-	if ctx == nil {
-		return nil, fmt.Errorf("AppContext not initialized")
-	}
-
-	switch serverType {
+func CreateMinecraftServer(config config.MinecraftServerConfig) (MinecraftServer, error) {
+	switch config.RuntimeType {
 	case "docker":
-		ctx := internal.Get()
-		dc := pkg.NewDockerCompose(ctx.Config.DockerComposePath)
+		dc := pkg.NewDockerCompose(config.Docker.ComposePath)
 		return mcserver.NewDockerMinecraftServer(dc, "mine_server"), nil
 	default:
-		return nil, fmt.Errorf("unknown pubsub type: %s", serverType)
+		return nil, fmt.Errorf("unknown pubsub type: %s", config.RuntimeType)
 	}
 }
