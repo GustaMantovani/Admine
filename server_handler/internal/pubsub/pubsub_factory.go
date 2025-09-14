3045efaf6081108ea124a28d684729d477f839ad
Gustavo Mantovani <mantovanig000@gmail.com>
2025-09-14 17:49:42 -0300

    ♻️ refactor package naming for pubsub implementation

--- server_handler/internal/pubsub/pubsub_factory.go
+++ server_handler/internal/pubsub/pubsub_factory.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"admine.com/server_handler/internal/config"
-	"admine.com/server_handler/internal/pubsub/pubsub_impls"
+	pubsub_impls "admine.com/server_handler/internal/pubsub/pubsub_impls"
 )
 
 // CreatePubSub returns a concrete PubSubService based on type
@@ -12,7 +12,7 @@ func CreatePubSub(c config.PubSubConfig) (PubSubService, error) {
 
 	switch c.Type {
 	case "redis":
-		return pubsub.NewRedisPubSub(c.Redis), nil
+		return pubsub_impls.NewRedisPubSub(c.Redis), nil
 
 	default:
 		return nil, fmt.Errorf("unknown pubsub type: %s", c.Type)
