6583ecb442ef143ea25b509ef4d498e88824aa44
Gustavo Mantovani <mantovanig000@gmail.com>
2025-10-11 23:17:44 -0300

    :fire:

--- bot/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Shared configurations for Admine Bot project tests."""
-
-import sys
-from pathlib import Path
-
-# Add src directory to path to allow module imports
-src_path = Path(__file__).parent.parent / "src"
-sys.path.insert(0, str(src_path))
